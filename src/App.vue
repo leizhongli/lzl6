3756e774dacf480aea3b74a8ddd36004c17bcd68
leizhongli <lzlcyb1010@163.com>
2016-10-09 13:59:27 +0800

    我的Appp

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="app"> 
+		 <router-view></router-view>
+	</div>
+ 
+</template>
+
+<script>
+
+</script>
+
+<style>
+*{
+	margin:0;
+	padding:0;
+	-webkit-box-sizing:border-box;
+}
+html,body{
+	height:100%;
+	width:100%;
+
+}
+#app{
+	height: 100%;
+	width:100%;
+	overflow: hidden;
+	display:-webkit-box;
+	-webkit-box-orient:vertical;
+}
+
+</style>
